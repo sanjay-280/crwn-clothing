dd9dd56f57098e2d77e89ac4186d652fd9ca2968
Sanjay-Arw <sanjay@hiranya-garbha.com>
2021-01-18 20:18:49 +0530

    implemented react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatPage} />
+      </Switch>
     </div>
   );
 }
